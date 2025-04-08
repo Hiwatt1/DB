9f2abd311bf1c121b85fc01acbad0c3c3d0cc6d8
Hiwatt1 <btwins@bk.ru>
2025-04-08 17:39:56 +0300

    1

--- app/models.py
+++ app/models.py
@@ -22,3 +22,12 @@ class BlogPost(Base):
     content = Column(String)
     date = Column(Date)
 
+from sqlalchemy import Column, Integer, String
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    password_hash = Column(String)
+
--- app/schemas.py
+++ app/schemas.py
@@ -28,3 +28,17 @@ class BlogPost(BlogPostCreate):
     class Config:
         orm_mode = True
 
+from pydantic import BaseModel
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+class UserOut(BaseModel):
+    id: int
+    username: str
+
+    class Config:
+        orm_mode = True
+
+
